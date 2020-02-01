97a3d69d20d7b99f29fbe68b34a8f9e2f49626eb
Jacob Harrington <jharrington4@gmail.com>
2020-02-01 10:30:54 -0800

    added basic style scaffolding to Resume container

--- src/containers/Resume.js
+++ src/containers/Resume.js
@@ -7,6 +7,7 @@ import TechStack from '../components/resume/TechStack.js';
 import Projects from '../components/resume/Projects.js';
 import Works from '../components/resume/Works.js';
 import Schools from '../components/Schools.js';
+import styles from './Resume.css';
 
 export default class Resume extends Component{
 
@@ -14,19 +15,21 @@ export default class Resume extends Component{
     const { header, contacts, techStack } = data;
 
     return (
-      <>
+      <article className={styles.Article}>
         <Header {...header} />
-        <section>
-          <PersonalProfile {...data} />
-          <Contacts {...contacts} />
-          <TechStack {...techStack} />
-        </section>
-        <section>
-          <Projects {...data} />
-          <Works {...data} />
-          <Schools {...data} />
-        </section>
-      </>
+        <main className={styles.Main}>
+          <section className={styles.sectionOne}>
+            <PersonalProfile {...data} />
+            <Contacts {...contacts} />
+            <TechStack {...techStack} />
+          </section>
+          <section className={styles.sectionTwo}>
+            <Projects {...data} />
+            <Works {...data} />
+            <Schools {...data} />
+          </section>
+        </main>
+      </article>
     );
   }
 }
